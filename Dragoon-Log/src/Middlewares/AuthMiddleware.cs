5734bf64968cda27bd65ec61b421d81297fa8a5f
Ives Costa <ivespauiniam@gmail.com>
2023-03-25 22:48:56 -0500

    ajustando

--- Dragoon-Log/src/Middlewares/AuthMiddleware.cs
+++ Dragoon-Log/src/Middlewares/AuthMiddleware.cs
@@ -21,21 +21,21 @@ public class AuthenticationMiddleware
     
     public async Task InvokeAsync(HttpContext httpContext)
     {
-        var password = httpContext.Request.Headers.FirstOrDefault(
-            x => x.Key == "SecretKey").Value.FirstOrDefault();
-        var clientId = httpContext.Request.Headers.FirstOrDefault(
-            x => x.Key == "ClientId").Value;
-        var clientSecret = httpContext.Request.Headers.FirstOrDefault(
-            x => x.Key == "ClientSecret").Value;
-        var result = await _repo.Filter(clientId, clientSecret);
-        if (result.Count == 0 && password != Config.PASSWORD)
-        {
-            httpContext.Response.StatusCode = 401;
-            
-            var json = JsonConvert.SerializeObject(Responses.Forbiden);
-            await httpContext.Response.WriteAsync(json);
-            return;
-        }
+        // var password = httpContext.Request.Headers.FirstOrDefault(
+        //     x => x.Key == "SecretKey").Value.FirstOrDefault();
+        // var clientId = httpContext.Request.Headers.FirstOrDefault(
+        //     x => x.Key == "ClientId").Value;
+        // var clientSecret = httpContext.Request.Headers.FirstOrDefault(
+        //     x => x.Key == "ClientSecret").Value;
+        // var result = await _repo.Filter(clientId, clientSecret);
+        // if (result.Count == 0 && password != Config.PASSWORD)
+        // {
+        //     httpContext.Response.StatusCode = 401;
+        //     
+        //     var json = JsonConvert.SerializeObject(Responses.Forbiden);
+        //     await httpContext.Response.WriteAsync(json);
+        //     return;
+        // }
         await _next(httpContext);
     }
 }
