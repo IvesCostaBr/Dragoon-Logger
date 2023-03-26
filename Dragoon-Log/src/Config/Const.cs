c6f085d51508952a5c1839da3549d8e1e990f844
Ives Costa <ivespauiniam@gmail.com>
2023-03-25 22:35:20 -0500

    Justes

--- Dragoon-Log/src/Config/Const.cs
+++ Dragoon-Log/src/Config/Const.cs
@@ -1,6 +1,12 @@
 public class Config
 {
-    public static string DATABASE_URI   { get { return Environment.GetEnvironmentVariable("DATABASE_URI"); } }
+    public static string DATABASE_URI
+    {
+        get
+        {
+            return "mongodb+srv://recharge:recharge%402022@cluster0.ri2aos7.mongodb.net/?retryWrites=true&w=majority";
+        }
+    }
     public static string DATABASE   { get { return Environment.GetEnvironmentVariable("DATABASE"); } }
     public static int SOCKET_PORT   { get { return int.Parse(Environment.GetEnvironmentVariable("SOCKET_PORT")); } }
     public static string PASSWORD   { get { return Environment.GetEnvironmentVariable("PASSWORD"); } }
